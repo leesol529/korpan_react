b508cdbab17dd1862eb1b6c4c93518aa0b4d698a
roorooro1 <roorooro9933@gmail.com>
2020-01-30 15:09:05 +0900

    scss 설정

--- src/router/AppRouter.js
+++ src/router/AppRouter.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Home from '../component/Home';
+import Header from '../component/Header';
 
 const AppRouter = () => (
     <BrowserRouter>
         <div>
+            <Header />
             <Switch>
                 <Route path="/" component={Home} exact={true} />
             </Switch>
