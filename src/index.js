3e16852651dd7f0930f5068c2ef194c2274a9fa7
roorooro1 <roorooro9933@gmail.com>
2020-01-30 15:26:17 +0900

    scss 설정 완료

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
 import AppRouter from './router/AppRouter';
+import './styles/styles.scss';
 
 ReactDOM.render(<AppRouter />, document.getElementById('root'));
 
